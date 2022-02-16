c28a6d49857b873af033c8895aff4373c512d779
User <user@Users-MacBook-Air.local>
2022-02-16 16:24:16 +0100

    refactor some stuff

--- src/main/java/com/store/customerOperations/CustomerOperationsImpl.java
+++ src/main/java/com/store/customerOperations/CustomerOperationsImpl.java
@@ -5,31 +5,30 @@ import com.store.exceptions.ProductOutOfStockException;
 import com.store.models.Customer;
 import com.store.models.Product;
 import com.store.models.Store;
-
 import java.util.Map;
-
 public class CustomerOperationsImpl implements CustomerOperations{
 
     @Override
     public Map<Product, Integer> buyProduct(Customer customer, Store store, Product product, int quantityWanted) throws ProductOutOfStockException, ProductNotInStockException {
-         Map<Product, Integer> map = store.getProductMap();
+         Map<Product, Integer> productMapPlaceHolder = store.getProductMap();
 
-             if (map.containsKey(product) && map.get(product) >= quantityWanted) {
+             if (productMapPlaceHolder.containsKey(product) && productMapPlaceHolder.get(product) >= quantityWanted) {
                  if (customer.getCartMap().containsKey(product)) {
-                     int i = customer.getCartMap().get(product) + quantityWanted;
-                     if (i > map.get(product) ) {
+                     int totalQuantityWanted = customer.getCartMap().get(product) + quantityWanted;
+                     if (totalQuantityWanted > productMapPlaceHolder.get(product) ) {
 
                          throw new ProductOutOfStockException("Sorry, we don't have up to " + quantityWanted + " " + product.getProductName() +" left in stock");
                      } else {
-                         customer.getCartMap().put(product,i);
+                         customer.getCartMap().put(product,totalQuantityWanted);
+
                      }
 
                  }else {
                      customer.getCartMap().put(product,quantityWanted);
                  }
 
-             } else if (map.containsKey(product) && map.get(product) < quantityWanted) {
-                 throw new ProductOutOfStockException("Sorry, we only have " + map.get(product) + " in stock");
+             } else if (productMapPlaceHolder.containsKey(product) && productMapPlaceHolder.get(product) < quantityWanted) {
+                 throw new ProductOutOfStockException("Sorry, we only have " + productMapPlaceHolder.get(product) + " in stock");
              } else {
                  throw new ProductNotInStockException("We don't have "  + product.getProductName());
              }
